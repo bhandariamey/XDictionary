11854122a1149fec01e131ebfc73ed924e729b02
bhandariamey <bhandariamey@gmail.com>
2024-03-27 15:55:30 +0530

    fc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [userWord, setUserWord] = useState("")
+  const [searchedData, setSearchedData] = useState([{'a':''}])
+
+  const data = [
+
+    { word: "React", meaning: "A JavaScript library for building user interfaces." },
+
+    { word: "Component", meaning: "A reusable building block in React." },
+
+    { word: "State", meaning: "An object that stores data for a component." }
+
+]
+
+  const handleSearch = () => {
+    setSearchedData(data.filter((ele)=>
+      ele.word.toLowerCase() === userWord.toLowerCase()
+    ))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      {console.log("SearchedData",searchedData)}
+      <h1>Dictionary App</h1>
+      <input onChange={(e)=>{setUserWord(e.target.value)}} type='text' placeholder='Search for a word...' value={userWord} />
+      <button onClick={handleSearch}>Search</button>
+      <h3>Definition:</h3>
+      {Object.keys(searchedData).length!==1 ? <p >Word not found in the dictionary.</p> : <p>{searchedData[0].meaning}</p>}
+    </>
   );
 }
 
